a2bab6d80a3dfa95b02c9bcfb95dcca80c19aeb8
Liana-V <52818570+Liana-V@users.noreply.github.com>
2020-08-08 11:29:51 +0400

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def create_db():
-    conn = sqlite3.connect("db")
-    cursor = conn.cursor()
-    with open("database.sql","r") as file:
-        cursor.executescript(file.read())
-    conn.close()
-    
